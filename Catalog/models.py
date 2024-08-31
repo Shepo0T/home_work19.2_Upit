c5f5c5db8b9365f92ab9ba4e6e5896b49e9c9fdd
Aleksandr <zemacohoznik@gmail.com>
2024-08-31 20:02:23 +0300

    Создание моделей Категории и Продукты

--- Catalog/models.py
+++ Catalog/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models import ImageField
+from django.views.decorators.http import last_modified
+
 
 class Task(models.Model):
     title = models.CharField('Название', max_length=50)
@@ -6,3 +9,35 @@ class Task(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Category(models.Model):
+    name = models.TextField(max_length='20', verbose_name='Наименование')
+    description = models.TextField(max_length='100', verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name = 'Категории'
+        ordering = ['name']
+
+
+
+class Product(models.Model):
+    name = models.TextField(max_length='20', verbose_name='Наименование')
+    description = models.TextField(max_length='200', verbose_name='Описание')
+    preview = models.ImageField(upload_to='products/foto', blank=True, null=True, verbose_name='Изображение(превью)')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    purchase_price = models.FloatField(verbose_name='Цена за покупку')
+    created_at = models.DateField(verbose_name='Дата создания(записи в БД)')
+    updated_at = models.DateField(verbose_name='Дата последнего изменения(записи в БД)')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ['name', 'updated_at', 'purchase_price', 'category', 'created_at']
