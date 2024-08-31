0ebbb8bec7baf5523d56137e07f3f0ce1d288766
Aleksandr <zemacohoznik@gmail.com>
2024-08-31 22:46:50 +0300

    Редактирование модели Product

--- Catalog/models.py
+++ Catalog/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 from django.db.models import ImageField
+from django.utils import timezone
 from django.views.decorators.http import last_modified
 
 
 class Task(models.Model):
-    title = models.CharField('Название', max_length=50)
-    task = models.TextField('Описание')
+    title = models.CharField("Название", max_length=50)
+    task = models.TextField("Описание")
 
     def __str__(self):
         return self.title
 
 
 class Category(models.Model):
-    name = models.TextField(max_length='20', verbose_name='Наименование')
-    description = models.TextField(max_length='100', verbose_name='Описание')
+    name = models.TextField(max_length="20", verbose_name="Наименование")
+    description = models.TextField(max_length="100", verbose_name="Описание")
 
     def __str__(self):
         return self.name
@@ -21,23 +22,40 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name = 'Категории'
-        ordering = ['name']
-
+        ordering = ["name"]
 
 
 class Product(models.Model):
-    name = models.TextField(max_length='20', verbose_name='Наименование')
-    description = models.TextField(max_length='200', verbose_name='Описание')
-    preview = models.ImageField(upload_to='products/foto', blank=True, null=True, verbose_name='Изображение(превью)')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
-    purchase_price = models.FloatField(verbose_name='Цена за покупку')
-    created_at = models.DateField(verbose_name='Дата создания(записи в БД)')
-    updated_at = models.DateField(verbose_name='Дата последнего изменения(записи в БД)')
+    name = models.TextField(max_length="20", verbose_name="Наименование")
+    description = models.TextField(max_length="200", verbose_name="Описание")
+    preview = models.ImageField(
+        upload_to="products/foto",
+        blank=True,
+        null=True,
+        verbose_name="Изображение(превью)",
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name="products",
+    )
+    purchase_price = models.FloatField(
+        verbose_name="Цена за покупку",
+    )
+    created_at = models.DateField(
+        verbose_name="Дата создания(записи в БД)",
+    )
+    updated_at = models.DateField(
+        verbose_name="Дата последнего изменения(записи в БД)",
+    )
+
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Продукт'
-        verbose_name_plural = 'Продукты'
-        ordering = ['name', 'updated_at', 'purchase_price', 'category', 'created_at']
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["name", "updated_at", "purchase_price", "category", "created_at"]
