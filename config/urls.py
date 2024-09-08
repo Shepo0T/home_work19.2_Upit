f046ac3dd0ecec16021d3a915f309da967e2da4b
Aleksandr <zemacohoznik@gmail.com>
2024-09-08 20:51:55 +0300

    Создание нового приложения

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Catalog.urls', namespace='Catalog'))
-
+    path('', include('Catalog.urls', namespace='Catalog')),
+    path('blog/', include('blog.urls', namespace='blog'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
