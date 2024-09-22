8cce24829e61155bc1bf49f06377c033c483d058
Aleksandr <zemacohoznik@gmail.com>
2024-09-22 22:17:16 +0300

    регистрация Версии

--- Catalog/admin.py
+++ Catalog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from Catalog.models import Product, Category
+from django.contrib.admin.templatetags.admin_urls import admin_urlname
+
+from Catalog.models import Product, Category, Version
 
 
 @admin.register(Category)
@@ -12,3 +14,9 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'purchase_price', 'category')
     list_filter = ('category',)
     search_fields = ('name', 'description')
+
+@admin.register(Version)
+class VersionAdmin(admin.ModelAdmin):
+    list_display = ('product', 'version_number', 'name_version', 'current_version')
+    list_filter = ('product',)
+    search_fields = ('product',)
