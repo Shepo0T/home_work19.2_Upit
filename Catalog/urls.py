f93f1effe77a36a103f00c1a89fbabb9f533b78d
Aleksandr <zemacohoznik@gmail.com>
2024-09-22 22:19:23 +0300

    Редактирование под CRUD и создание новых ссылок

--- Catalog/urls.py
+++ Catalog/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from Catalog.apps import CatalogConfig
-from Catalog.views import HomeListView, ContactsView, CatalogListView, ProductDetailView
+from Catalog.views import HomeListView, ContactsView, CatalogListView, ProductDetailView, ProductUpdate, \
+    ProductDeleteView, ProductCreateView, toggle_activity_product, CreateProductView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('',HomeListView.as_view(), name='home'),
-    path('catalog',CatalogListView.as_view(), name='catalog'),
-    path('contacts/',ContactsView.as_view(), name='contacts'),
-    path('product/<int:pk>',ProductDetailView.as_view(), name='product')
+    path('', HomeListView.as_view(), name='home'),
+    path('catalog', CatalogListView.as_view(), name='catalog'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('product/<int:pk>', ProductDetailView.as_view(), name='product'),
+    path('update_product/<int:pk>', ProductUpdate.as_view(), name='update_product'),
+    path('delete_product/<int:pk>', ProductDeleteView.as_view(), name='delete'),
+    path('create_product/', ProductCreateView.as_view(), name='create'),
+    path('create_product_view/', CreateProductView, name='create_product_view'),
+    path('activity_product/<int:pk>/', toggle_activity_product, name='toggle_activity_product')
 ]
