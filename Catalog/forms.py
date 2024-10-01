ca647099c9f5a33a6247c36f10a83de0c284d939
Aleksandr <zemacohoznik@gmail.com>
2024-10-01 18:29:17 +0300

    Создание формы для модератора

--- Catalog/forms.py
+++ Catalog/forms.py
@@ -42,3 +42,25 @@ class VersionForm(StyleFormMixin, forms.ModelForm):
     class Meta:
          model = Version
          fields = '__all__'
+
+class ProductModeratorForm(StyleFormMixin, forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = ('description', 'category', 'is_active')
+
+    def clean_name(self):
+        cleaned_name = self.cleaned_data['name']
+        for word in forbidden_words:
+            if  word in cleaned_name:
+                raise forms.ValidationError('Содержит запрещенные слова')
+
+        return cleaned_name
+
+    def clean_description(self):
+        cleaned_description = self.cleaned_data['description']
+        for word in forbidden_words:
+            if word in cleaned_description:
+                raise forms.ValidationError('Содержит запрещенные слова')
+
+        return cleaned_description
