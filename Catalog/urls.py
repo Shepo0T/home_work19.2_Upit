58280d84b1772af8616bb452c3de099b26a18e98
Aleksandr <zemacohoznik@gmail.com>
2024-10-05 20:33:58 +0300

    Настройка кеширования контроллера

--- Catalog/urls.py
+++ Catalog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 from Catalog.apps import CatalogConfig
 from Catalog.views import HomeListView, ContactsView, CatalogListView, ProductDetailView, ProductUpdate, \
-    ProductDeleteView, ProductCreateView, toggle_activity_product, ProductSaleView
+    ProductDeleteView, ProductCreateView, toggle_activity_product, ProductSaleView, CategoryListView
 
 app_name = CatalogConfig.name
 
@@ -9,10 +11,11 @@ urlpatterns = [
     path('', HomeListView.as_view(), name='home'),
     path('catalog', CatalogListView.as_view(), name='catalog'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
+    path('product/<int:pk>/', cache_page(120)(ProductDetailView.as_view()), name='product'),
     path('update_product/<int:pk>', ProductUpdate.as_view(), name='update_product'),
     path('delete_product/<int:pk>', ProductDeleteView.as_view(), name='delete'),
     path('create_product/', ProductCreateView.as_view(), name='create'),
     path('create_product_view/', ProductSaleView, name='create_product_view'),
-    path('activity_product/<int:pk>/', toggle_activity_product, name='toggle_activity_product')
+    path('activity_product/<int:pk>/', toggle_activity_product, name='toggle_activity_product'),
+    path('category_list/', CategoryListView.as_view(), name='category_list')
 ]
